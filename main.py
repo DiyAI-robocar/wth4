3754ae39df65d375d04ed8a051014a68647e118d
kurbakov <kurbakov.d@gmail.com>
2018-10-23 23:17:31 +0200

    my own map

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from threading import Thread, Lock
 import ast
 import socket
 
+UDP_IP = "127.0.0.1"
+UDP_PORT = 2115
+
 
 class WTH4:
 	"""
@@ -37,7 +40,7 @@ class WTH4:
 		self.thread_socket.daemon = True
 
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.sock.bind(('127.0.0.1', 5005))
+		self.sock.bind((UDP_IP, UDP_PORT))
 		self.sock.listen(1)
 		self.sock.settimeout(2)
 
@@ -50,7 +53,11 @@ class WTH4:
 				self.socket_data = self.sock.recvmsg(1024)
 				if self.socket_data != 0:
 					coordinates = self.socket_data.split()
-					self.match(coordinates[0], coordinates[1])
+					road_points, building_points = self.match(coordinates[0], coordinates[1])
+					data = dict()
+					data["road"] = road_points
+					data["buildings"] = building_points
+					self.sock.sendto(bytes(data, "ascii"), (UDP_IP, UDP_PORT))
 			except:
 				print("Localisation service is not available")
 
@@ -60,19 +67,25 @@ class WTH4:
 
 	def match(self, longitude, latitude):
 		result = []
+		buildings = []
 
 		if not self.map:
 			return result
 
 		self.mutex.acquire()
 		try:
-			for point in self.map.get("features").get("geometry").get("coordinates"):
+			for point in self.map.get("features").get("geometry").get("road_coordinates"):
 				# 200 is the number that still need to validate!!!
 				if self.distance(longitude, latitude, point[0], point[1]) <= 200:
 					result.append(point)
+			for building in self.map.get("features").get("geometry").get("building_coordinates"):
+				# remove it or check
+				if self.distance(longitude, latitude, building[0], building[1]) <= 300:
+					buildings.append(building)
+
 		finally:
 			self.mutex.release()
-			return result
+			return result, buildings
 
 	def shutdown(self):
 		print ("Shutdown call...")
