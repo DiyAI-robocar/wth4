47728c6cc37a778cc5cb91a73d895fa10d792d95
Michal Slonina <michal.slonina@gmail.com>
2018-10-24 00:01:32 +0200

    fixed the server

--- main.py
+++ main.py
@@ -3,102 +3,97 @@ import time
 from threading import Thread, Lock
 import ast
 import socket
+import os 
+import json
+import math
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 2115
+UDP_PORT_SEND = 2116
 
 
 class WTH4:
-	"""
-	Class to update the map file
-	"""
+    """
+    Class to update the map file
+    """
 
-	def __init__(self):
-		self.on = True
-		self.map = None
-		self.socket_data = None
-		self.mutex = Lock()
+    def __init__(self):
+        self.on = True
+        self.map = None
+        self.socket_data = None
+        self.mutex = Lock()
 
-	def initiate_map_update(self, path):
-		self.github_file = path
-		self.thread_map = Thread(target = self.start_map_update)
-		self.thread_map.daemon = True
-		self.thread_map.start()
+    def initiate_map_update(self, path):
+        self.github_file = path
+        self.thread_map = Thread(target=self.start_map_update)
+        self.thread_map.daemon = True
+        self.thread_map.start()
 
-	def start_map_update(self):
-		while self.on:
-			response = requests.get(self.github_file).text
-			self.mutex.acquire()
-			try:
-				self.map = ast.literal_eval(response)
-			finally:
-				self.mutex.release()
-			time.sleep(5)
+    def start_map_update(self):
+        while self.on:
+            with open('map.json') as f:
+                self.map = json.load(f)
+            time.sleep(5)
 
-	def initiate_socket(self):
-		self.thread_socket = Thread(target=self.start_recvmsg)
-		self.thread_socket.daemon = True
+    def initiate_socket(self):
+        self.thread_socket = Thread(target=self.start_recvmsg)
+        self.thread_socket.daemon = True
 
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.sock.bind((UDP_IP, UDP_PORT))
-		self.sock.listen(1)
-		self.sock.settimeout(2)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((UDP_IP, UDP_PORT))
 
-		self.thread_socket.start()
+        self.sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-	def start_recvmsg(self):
-		while self.on:
-			try:
-				self.sock.accept()
-				self.socket_data = self.sock.recvmsg(1024)
-				if self.socket_data != 0:
-					coordinates = self.socket_data.split()
-					road_points, building_points = self.match(coordinates[0], coordinates[1])
-					data = dict()
-					data["road"] = road_points
-					data["buildings"] = building_points
-					self.sock.sendto(bytes(data, "ascii"), (UDP_IP, UDP_PORT))
-			except:
-				print("Localisation service is not available")
+        self.thread_socket.start()
 
-	@staticmethod
-	def distance(x1, y1, x2, y2):
-		return pow((x1-x2), 2) + pow((y1-y2), 2)
+    def start_recvmsg(self):
+        while self.on:
+            self.socket_data = self.sock.recvmsg(1024)
+            if self.socket_data != 0:
+                coordinates = self.socket_data[0].split()
+                coordinates = [float(coordinates[0]),float(coordinates[1])]
+                road_points, building_points = self.match(
+                    coordinates[0], coordinates[1])
+                data = dict()
+                data["road"] = road_points
+                data["buildings"] = building_points
+                self.sock_send.sendto(bytes(json.dumps(data), "ascii"), (UDP_IP, UDP_PORT_SEND))
 
-	def match(self, longitude, latitude):
-		result = []
-		buildings = []
+    @staticmethod
+    def distance(x1, y1, x2, y2):
+        return math.sqrt(pow((x1-x2), 2) + pow((y1-y2), 2))
 
-		if not self.map:
-			return result
+    def match(self, longitude, latitude):
+        result = []
+        buildings = []
 
-		self.mutex.acquire()
-		try:
-			for point in self.map.get("features").get("geometry").get("road_coordinates"):
-				# 200 is the number that still need to validate!!!
-				if self.distance(longitude, latitude, point[0], point[1]) <= 200:
-					result.append(point)
-			for building in self.map.get("features").get("geometry").get("building_coordinates"):
-				# remove it or check
-				if self.distance(longitude, latitude, building[0], building[1]) <= 300:
-					buildings.append(building)
+        if not self.map:
+            return result
 
-		finally:
-			self.mutex.release()
-			return result, buildings
+        print("TEXT")
+        for point in self.map.get("features")[0].get("geometry").get("road_coordinates"):
+            # 200 is the number that still need to validate!!!
+            print(self.distance(longitude, latitude, point[0], point[1]))
+            if self.distance(longitude, latitude, point[0], point[1]) <= 400:
+                result.append(point)
+        for building in self.map.get("features")[0].get("geometry").get("building_coordinates"):
+            # remove it or check
+            if self.distance(longitude, latitude, building[0], building[1]) <= 500:
+                buildings.append(building)
 
-	def shutdown(self):
-		print ("Shutdown call...")
-		self.on = False
-		self.sock.close()
-		self.thread_map.join()
-		self.thread_socket.join()
+        return result, buildings
+
+    def shutdown(self):
+        print("Shutdown call...")
+        self.on = False
+        self.sock.close()
+        self.thread_map.join()
+        self.thread_socket.join()
 
 
 if __name__ == "__main__":
-	stuff = WTH4()
-	stuff.initiate_map_update(path="https://raw.githubusercontent.com/nai1gun/robocar-map/master/map/track.geojson")
-	stuff.initiate_socket()
-
-	stuff.shutdown()
-
+    stuff = WTH4()
+    stuff.initiate_map_update(
+        path="https://raw.githubusercontent.com/nai1gun/robocar-map/master/map/track.geojson")
+    stuff.initiate_socket()
+    os.system('read x "Press any key to continue..."')
